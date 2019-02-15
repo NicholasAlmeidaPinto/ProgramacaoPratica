76fee0116b9a035599bf577eec1057ada04cb7e1
NicholasAlmeida <n.i.cholas@hotmail.com>
2019-02-15 19:31:57 -0200

    New changes!

--- Codes/CReservatorio.cpp
+++ Codes/CReservatorio.cpp
@@ -1,31 +1,29 @@
 #include "CReservatorio.h"
 
 void CReservatorio::LerDados(std::string NomeArquivo) {
-	NomeArquivo = NomeArquivo+".txt";
+	//NomeArquivo = NomeArquivo+".txt";
 	std::ifstream f(NomeArquivo);
 	std::string line;
 	int a;
 
-	while (!f.eof()) {
-		std::getline(f, line); std::getline(f, line); PressaoInicial = std::stof(line);
-		std::getline(f, line); std::getline(f, line); Vazao = std::stof(line);
-		std::getline(f, line); std::getline(f, line); Permeabilidade = std::stof(line);
-		std::getline(f, line); std::getline(f, line); Viscosidade = std::stof(line);
-		std::getline(f, line); std::getline(f, line); RaioPoco = std::stof(line);
-		std::getline(f, line); std::getline(f, line); FatorVolForm = std::stof(line);
-		std::getline(f, line); std::getline(f, line); ComprTotal = std::stof(line);
-		std::getline(f, line); std::getline(f, line); Porosidade = std::stof(line);
-		std::getline(f, line); std::getline(f, line); AlturaReser = std::stof(line);
-		std::getline(f, line); std::getline(f, line); Latitude = std::stof(line);
-		std::getline(f, line); std::getline(f, line); Longitude = std::stof(line);
-		std::getline(f, line); std::getline(f, line); FatorPelicula = std::stof(line);
-		std::getline(f, line); std::getline(f, line); RaioExterno = std::stof(line);
-		std::getline(f, line); std::getline(f, line); FatorForma = std::stof(line);
-		std::getline(f, line); std::getline(f, line); Area = std::stof(line);
-		std::getline(f, line); std::getline(f, line); a = std::stoi(line);
-		if (a == 0)
-			anp = true;
-		else
-			anp = false;
-	}
+	std::getline(f, line); std::getline(f, line); PressaoInicial = std::stod(line);
+	std::getline(f, line); std::getline(f, line); Vazao = std::stod(line);
+	std::getline(f, line); std::getline(f, line); Permeabilidade = std::stod(line);
+	std::getline(f, line); std::getline(f, line); Viscosidade = std::stod(line);
+	std::getline(f, line); std::getline(f, line); RaioPoco = std::stod(line);
+	std::getline(f, line); std::getline(f, line); FatorVolForm = std::stod(line);
+	std::getline(f, line); std::getline(f, line); ComprTotal = std::stod(line);
+	std::getline(f, line); std::getline(f, line); Porosidade = std::stod(line);
+	std::getline(f, line); std::getline(f, line); AlturaReser = std::stod(line);
+	std::getline(f, line); std::getline(f, line); Latitude = std::stod(line);
+	std::getline(f, line); std::getline(f, line); Longitude = std::stod(line);
+	std::getline(f, line); std::getline(f, line); FatorPelicula = std::stod(line);
+	std::getline(f, line); std::getline(f, line); RaioExterno = std::stod(line);
+	std::getline(f, line); std::getline(f, line); FatorForma = std::stod(line);
+	std::getline(f, line); std::getline(f, line); Area = std::stod(line);
+	std::getline(f, line); std::getline(f, line); a = std::stoi(line);
+	if (a == 0)
+		anp = true;
+	else
+		anp = false;
 }
--- Codes/main.cpp
+++ Codes/main.cpp
@@ -6,9 +6,11 @@ using namespace std;
 
 int main()
 {
-	string nome = "Reservatorio0";
+
+	string nome = "C:/Users/nicho/Source/Repos/NicholasAlmeidaPinto/ProgramacaoPratica/Codes/Dados/Reservatorio0.txt";
 	CPressao pressao(nome);
 	cout << "\nQueda de pressao: " << pressao.ReservatorioInfinito(100,10);
 	cin.get();
+	
 	return 0;
 }
